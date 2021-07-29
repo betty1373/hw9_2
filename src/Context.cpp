66432f9e3429be0c54f15c3bf0770ecb9c71a1c4
betty1373 <betty1373@mail.ru>
2021-07-29 14:55:01 +0300

    End part 2

--- src/Context.cpp
+++ src/Context.cpp
@@ -1,20 +1,19 @@
 #include "../inc/Context.h"
-
   Context::Context(std::size_t numCmds)
      : m_stop{false},
        m_notify{false},
        m_thread {&Context::Work,this} {
-    m_cmdReader =  CmdReader::Create(static_cast<size_t>(numCmds));
+   // m_cmdReader =  CmdReader::Create(static_cast<size_t>(numCmds));
      
-    m_consoleLogger = ConsoleLogger::Create("log",m_cmdReader);
-    m_fileLogger = FileLogger::Create("file",m_cmdReader);
+   // m_consoleLogger = ConsoleLogger::Create("log",m_cmdReader);
+   // m_fileLogger = FileLogger::Create("file",m_cmdReader);
     
-    m_cmdReader->SetContext(this);
-    m_consoleLogger->SetContext(this);
-    m_fileLogger->SetContext(this);
-    std::cout<<"Construct Context"<<std::endl;
-    
+    //m_cmdReader->SetContext(this);
+   // m_consoleLogger->SetContext(this);
+   // m_fileLogger->SetContext(this);
+    std::cout<<"Construct Context"<<std::endl;    
   }
+
   Context::~Context() {
       StopWork();
        std::cout<<"Destroy Context"<<std::endl;
@@ -37,7 +36,7 @@
       m_cv.wait(locker, [&](){return m_notify || m_stop;});
       std::string cmd;
       while (std::getline(m_strstream, cmd) ) {
-        m_cmdReader->NewCmd(cmd);
+       // m_cmdReader->NewCmd(cmd);
       }
       m_strstream.clear();
       m_strstream.str("");
@@ -51,4 +50,4 @@
         if (m_thread.joinable()) {
             m_thread.join();
         }
-   };
+  };
